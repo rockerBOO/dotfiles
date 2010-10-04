d39cb810ae11689169938a228d488cbe272e3e9c
Brett Buddin <brett@intraspirit.net>
2010-10-04 14:16:12 -0400

    Removing some alias I never use and changing my status one.

--- zsh/lib/aliases.zsh
+++ zsh/lib/aliases.zsh
@@ -7,17 +7,14 @@ alias ld='ls -ld *(/)'
 
 alias rmswp='find . -name "*.swp" -type f | xargs rm -f'
 
-alias gst='git status'
+alias gs='git status'
 alias gl='git log'
 alias gf='git fetch'
 alias gb='git branch'
 alias gd='git diff'
 alias ga='git add'
 alias gc='git commit -m'
-alias gca='git commit -am'
 alias gcam='git commit --amend'
-alias gri='git rebase --interactive'
-alias gdc='git svn dcommit'
 
 alias rs='rails server'
 alias rc='rails console'
