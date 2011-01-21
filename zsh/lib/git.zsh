e20f9a22efbbf6bb7691d2ccba4f7a3ed22bd6a4
Troy Thompson <troynt@gmail.com>
2011-01-21 14:45:05 -0500

    cache git info to improve speed when executing commands that do not effect the git

--- zsh/lib/git.zsh
+++ zsh/lib/git.zsh
@@ -1,4 +1,36 @@
+# Tell zsh to execute function found in some magic arrays.
+# Each of these is an array of function names.
+# Each function name is executed when zsh performs some action.
+typeset -ga preexec_functions
+typeset -ga precmd_functions
+typeset -ga chpwd_functions
+
+# used to cache git info
+export __CURRENT_GIT_INFO=
+
+parse_git_branch() {
+  git branch --no-color 2> /dev/null \
+  | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'
+}
+
+preexec_functions+='zsh_preexec_update_git_vars'
+zsh_preexec_update_git_vars() {
+  case "$(history $HISTCMD)" in 
+    *git*)
+    export __CURRENT_GIT_INFO="$(update_git_prompt_info)"
+    ;;
+  esac
+}
+
+chpwd_functions+='zsh_chpwd_update_git_vars'
+zsh_chpwd_update_git_vars() {
+  export __CURRENT_GIT_INFO="$(update_git_prompt_info)"
+}
 function git_prompt_info {
+  echo "$__CURRENT_GIT_INFO"
+}
+
+function update_git_prompt_info {
   ref=$(git symbolic-ref HEAD 2> /dev/null) || return
   echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
 }
