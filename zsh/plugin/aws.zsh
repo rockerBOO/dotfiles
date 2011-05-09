3f3accaa794de82dd0831a1c6e0bc187b123fddd
Brett Buddin <brett@intraspirit.net>
2011-05-09 15:49:22 -0400

    AWS stuff

--- zsh/plugin/aws.zsh
+++ zsh/plugin/aws.zsh
@@ -1,3 +1,8 @@
-export JAVA_HOME="/Library/Java/Home/"
 export AWS_IAM_HOME="/usr/local/Cellar/aws-iam-tools/HEAD/jars"
 export AWS_CREDENTIAL_FILE=$HOME/.aws-credentials-master
+
+export JAVA_HOME="/System/Library/Frameworks/JavaVM.framework/Home"
+export EC2_PRIVATE_KEY="$(/bin/ls $HOME/.ec2/pk-*.pem)"
+export EC2_CERT="$(/bin/ls $HOME/.ec2/cert-*.pem)"
+export EC2_AMITOOL_HOME="/usr/local/Cellar/ec2-ami-tools/1.3-45758/jars"
+export EC2_HOME="/usr/local/Cellar/ec2-api-tools/1.3-62308/jars"
