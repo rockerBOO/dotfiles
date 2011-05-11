6340d319ec4a6c9a003761db96861543b687d819
Brett Buddin <brett@intraspirit.net>
2011-05-11 10:40:13 -0400

    Moving around the cd stack

--- zsh/lib/aliases.zsh
+++ zsh/lib/aliases.zsh
@@ -1,6 +1,17 @@
 alias ..='cd ..'
 alias ...='cd ../..'
+
 alias back='cd -'
+alias 1='cd -'
+alias 2='cd +2'
+alias 3='cd +3'
+alias 4='cd +4'
+alias 5='cd +5'
+alias 6='cd +6'
+alias 7='cd +7'
+alias 8='cd +8'
+alias 9='cd +9'
+
 alias l='ls -lah'
 alias ll='ls -lFh'
 alias ld='ls -ld *(/)'
