757e0ad4026eafd675498b20053a589ca1b0da7a
Brett Buddin <brett@intraspirit.net>
2011-06-05 23:37:23 -0400

    More contrast in the minimal theme.

--- zsh/themes/minimal.zsh
+++ zsh/themes/minimal.zsh
@@ -1,8 +1,8 @@
-PROMPT='%{$fg[yellow]%}(%c)%{$reset_color%} '
+PROMPT='%{$fg[cyan]%}[ %{$fg_bold[cyan]%}%c %{$fg_no_bold[cyan]%}]%{$reset_color%} '
 RPROMPT='$(git_prompt_info)'
 
-ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg[yellow]%}("
-ZSH_THEME_GIT_PROMPT_SUFFIX="%{$fg[yellow]%})%{$reset_color%}"
-ZSH_THEME_GIT_PROMPT_DIRTY=" %{$fg[red]%}!"
-ZSH_THEME_GIT_PROMPT_UNTRACKED=" %{$fg[red]%}?"
+ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg[magenta]%}"
+ZSH_THEME_GIT_PROMPT_SUFFIX="%{$fg[magenta]%}%{$reset_color%}"
+ZSH_THEME_GIT_PROMPT_DIRTY=" %{$fg_bold[yellow]%}!"
+ZSH_THEME_GIT_PROMPT_UNTRACKED=" %{$fg_bold[yellow]%}?"
 ZSH_THEME_GIT_PROMPT_CLEAN=""
