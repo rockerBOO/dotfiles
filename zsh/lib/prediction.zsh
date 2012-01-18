7e838752ba0a27e94d24d65da0a8cab0aa4dd819
Brett Buddin <brett@intraspirit.net>
2012-01-18 12:55:35 -0500

    Get out of here

--- zsh/lib/prediction.zsh
+++
@@ -1,9 +0,0 @@
-bindkey ^R history-incremental-search-backward
-setopt correct
-
-autoload predict-on
-zle -N predict-on
-zle -N predict-off
-bindkey ^Z   predict-on
-bindkey ^X^Z predict-off
-zstyle ':predict' verbose true
