8e33481a0dde0023dceb61e4b9a462bc489df259
rockerBOO <rockerboo@gmail.com>
2020-12-16 00:09:22 -0500

    Cleanup

--- config/nvim/after/ftplugin/lua.vim
+++ config/nvim/after/ftplugin/lua.vim
@@ -1,1 +1,2 @@
 setlocal tabstop=2 expandtab
+
--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,6 +1,6 @@
 --[[
 --  Hello
---]]
+--]] 
 
 local g, opt, cmd = vim.g, vim.o, vim.cmd
 
@@ -8,6 +8,10 @@ g.mapleader = " "
 
 opt.splitbelow = true -- Default split below
 opt.splitright = true -- Default split right
+
+opt.showmatch = true
+
+opt.pumblend = 10
 
 opt.laststatus = 2
 opt.ff = "unix"
@@ -17,28 +21,33 @@ cmd [[ set noshowmode ]]
 
 -- Files
 opt.hidden = true
-cmd [[ 
-set noswapfile
-set nobackup
-set nowritebackup 
-]]
+cmd [[ set noswapfile
+       set nobackup
+       set nowritebackup ]]
+opt.updatetime = 1000
+
+-- opt.shada = {"!", "'1000", "<50", "s10", "h"}
 
 -- Tabs
 opt.tabstop = 2
 opt.shiftwidth = 2
 opt.softtabstop = 2
-cmd [[
-set noexpandtab
-]]
+cmd [[ set noexpandtab ]]
 
-opt.termguicolors = true -- Support 24bit colors
 opt.clipboard = "unnamedplus"
 opt.scrolloff = 10 -- Leave 10 rows when scrolling
 opt.emoji = true -- Use emojis
 
+opt.formatoptions = "cqrnj"
+
 -- Completion
 opt.completeopt = "menuone,noinsert,noselect"
 opt.shortmess = "c" -- Avoid showing extra messages when using completion
+
+-- Colors
+opt.termguicolors = true -- Support 24bit colors
+
+-- cmd [[ color boo ]]
 
 -- Tmux support
 g["&t_8f"] = "<Esc>[38;2;%lu;%lu;%lum]"
@@ -51,9 +60,7 @@ g.ale_disable_lsp = 1 -- Disable Ale LSP support
 g.mix_format_on_save = 1 -- Elixir mix format on save
 g.rustfmt_autosave = 1 -- rustfmt on save
 
--- cmd [[ color boo ]]
-
--- Reload module after saving
+-- Highlight yank'd text after yankin' 
 cmd [[ augroup YankHighlight ]]
 cmd [[  autocmd! ]]
 cmd [[  autocmd TextYankPost *  lua vim.highlight.on_yank {higroup="IncSearch", timeout=1000} ]]
@@ -65,9 +72,14 @@ cmd [[  au! ]]
 cmd [[  au BufWritePost ~/.config/nvim/init.lua <cmd>lua require'plenary.reload'.reload_module'init'<cr> ]]
 cmd [[ augroup end ]]
 
--- Setup all the plugins
-require"plugins".setup()
+-- Setup key mappings
 require"mappings".setup()
 
+-- Setup all the plugins
+require"plugins".setup()
+
+-- Configure plugins
 require "setup"
+
+-- Colorscheme
 require"boo-colorscheme".use {}
